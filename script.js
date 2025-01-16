3298b43fae69260186cf24be68379ac2eda0d6c1
jesujaimes <jandresjaimes@mail.uniatlantico.edu.co>
2025-01-16 10:52:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".multi-tex",{
+    strings:["profesor de matematicas", "profesor de fisica", "profesor de informatica"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
